82d8f805a050b4762b83fbd93567544fb79db8e8
Scooom <me@scooom.com>
2026-05-12 23:17:23 +0000

    fix: correct title syntax in layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>WikiMoDex@hasSection('title') — @yield('title')@endif</title>
+    <title>WikiMoDex{{ View::hasSection('title') ? ' — ' . View::yieldContent('title') : '' }}</title>
 
     @stack('head')
 
