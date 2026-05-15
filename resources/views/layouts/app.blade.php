c1d3f07927a782bd4716e03c1b975cf0fc512bb7
WikiMoDex Dev <dev@wikimovoid>
2026-05-15 23:00:40 +0000

    Revert "feat: add Google Analytics tag, production only"
    This reverts commit 984addb5e8b15e641952aa2626d77d26422259c7.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,17 +16,6 @@
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
-
-    @if(app()->environment('production'))
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-QS5H466REX"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-QS5H466REX');
-    </script>
-    @endif
 </head>
 <body>
     @include('partials.nav')
